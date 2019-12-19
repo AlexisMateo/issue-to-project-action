7f0de97444d7ee1a9d48322553046c4ed87c9d20
Alexis Mateo <alexismateod@outlook.com>
2019-12-19 14:19:56 -0400

    test

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if(process.env.NODE_ENV=='development'){
+  require('dotenv').config();
+}
 const core = require('@actions/core');
 const github = require('@actions/github');
 
