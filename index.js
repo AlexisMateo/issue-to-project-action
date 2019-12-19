e042d384d983d5c3bd37ebd310cadc50623e14d7
Alexis Mateo <alexismateod@outlook.com>
2019-12-19 14:00:19 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+const core = require('@actions/core');
+const github = require('@actions/github');
+const githubClient = new github.GitHub(process.env.GITHUB_TOKE);
+const PROJECT_URL = process.env.PROJECT_URL;
+const API_PROJECT_URL = 'https://api.github.com/users/alexismateo/projects';
+const CARD_NAME = 'Issue';
+
+async function main(){
+    try {
+      await getIssues();
+        let project = await getProject();
+        addCardToProject(project);
+    
+    } catch (error) {
+      core.setFailed(error.message);
+    }
+
+}
+
+async function getProject(){
+  let projectId= parseInt(getProjectId(PROJECT_URL));
+  let project = await githubClient.projects.get({url:API_PROJECT_URL,project_id:projectId});
+  return project.data[0];
+}
+
+function getProjectId(PROJECT_URL){
+  let projectId = PROJECT_URL.match(/.\/?$/g)[0];
+  return projectId
+}
+
+function findCardByName(projectCards){
+  return projectCards.data.find(card=>card.name==CARD_NAME);
+}
+async function getIssues(){
+ let issues = await githubClient.repos.get({url:"https://api.github.com/repos/:owner/:repo/issues",owner:"alexismateo",repo:"vera"});
+ return issues;
+}
+
+async function addCardToProject(project) {
+  let projectId = project.id;
+  let projectCards = await githubClient.projects.get({url:`https://api.github.com/projects/${projectId}/columns`,project_id:projectId});
+  let card =  findCardByName(projectCards);
+
+  await githubClient.projects.createCard({
+    url:card.cards_url,
+    column_id:card.id,
+    note:"nasty",
+    content_type:"Issue",
+  });
+
+}
+main();
